f0ff75c844e385bd2a219618f243591246c7fde0
Agnes Kim <agneskim4@gmail.com>
2023-07-25 09:37:14 -0400

    Revert "changes to home app"
    This reverts commit 87c942115e6b309329a35d2d78060c80bdbefe2a.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 function App() {
     return (
+<<<<<<< HEAD
         <Router className="router">
             <Navbar className= "nav" />
+=======
+        <Router>
+            <Navbar />
+>>>>>>> parent of 87c9421 (changes to home app)
             <Routes>
                 <Route exact path='/' exact element={<Home />} />
                 <Route path='/album' element={<Album />} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,25 @@
 import React from "react";
-import {NavLink }from "react-router-dom";
-import './navbar.css'
+import { Nav, NavLink, NavMenu }from "./NavBarElements";
 
 const Navbar = () => {
     return (
         <>
+<<<<<<< HEAD
             <nav>
                 <div className="nav-container">
                     <NavLink className= "nav-link" to="/album" activeStyle>
+=======
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/album" activeStyle>
+>>>>>>> parent of 87c9421 (changes to home app)
                         Album
                     </NavLink>
-                    <NavLink className= "nav-link" to="/song" activeStyle>
+                    <NavLink to="/song" activeStyle>
                         Song
                     </NavLink>
-                </div>
-            </nav>
+                </NavMenu>
+            </Nav>
         </>
     );
 };
