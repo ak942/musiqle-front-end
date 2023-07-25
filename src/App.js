8d207b930421cbdc925dfae4d329caf675506f8a
Agnes Kim <agneskim4@gmail.com>
2023-07-25 09:36:34 -0400

    Revert "initial the front-end project"
    This reverts commit 26faba21f05a122caab2f88589df65e473820fee.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/NavBar/NavBar';
-import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
-import Home from './pages/Home';
-import Album from './pages/Album';
-import Song from './pages/Song';
-
 
 function App() {
     return (
