77ffb9f3a903756faa88ebf53f7a1f951237b2cd
Agnes Kim <agneskim4@gmail.com>
2023-07-27 19:46:21 -0400

    add audio element in song.js

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,12 +3,16 @@ import InputForm from './InputForm'
 import ScoreBoard from './ScoreBoard'
 
 const song = ({ name, currentScore, totalScore, streak, attempts = 4 }) => {
+    const play = () => {
+        new Audio('https://open.spotify.com/track/1dGr1c8CrMLDpV6mPbImSI').play()
+    }
     return (
         <div className="center game">
             <h1>Guess the Song</h1>
             <ScoreBoard currentScore={currentScore} totalScore={totalScore} streak={streak} />
             <p>Attempts Left: {attempts}</p>
             <div className="size"></div>
+            <button onClick={play}>Play Sound</button>
             <InputForm />
         </div>
     )
