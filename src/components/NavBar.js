f010c05e7ada98ce9f69c13a5fde5acdc6d49f24
Sydney Alberson <sydney.alberson@gmail.com>
2023-08-09 11:12:03 -0400

    Moved Dropdown component to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Nav, NavLink, NavMenu } from "./NavBarElements";
+import Dropdown from './Dropdown'
 import './navbar.css'
 
-const NavBar = () => {
+const NavBar = ({ genreOptions, genreChanged, selectedGenre, playlistOptions, playlistChanged, selectedPlaylist }) => {
     return (
         <Nav>
             <NavMenu className="nav-container">
                 <NavLink className="nav-link" to="/" activeStyle>
                     Return to Home
                 </NavLink>
+                <div className="dropdown-bar">
+                    <Dropdown
+                        options={genreOptions}
+                        changed={genreChanged}
+                        selected={selectedGenre}
+                    />
+                </div>
+                <div className="dropdown-bar">
+                    <Dropdown
+                        options={playlistOptions}
+                        changed={playlistChanged}
+                        selected={selectedPlaylist}
+                    />
+                </div>
             </NavMenu>
         </Nav>
     );
